304a1952735ebf495371b2d87f3a4d938e78f136
Muhammadjon <muhammadjon19.09.2008@gmail.com>
2022-08-06 12:47:00 +0300

    masala3

---
+++ main.js
@@ -0,0 +1,8 @@
+let number = +prompt(' 4 xonali son kiriting. ');
+number = number.toString();
+let birlik = +number[3];
+let onlik = +number[2];
+let yuzlik = +number[1];
+let minglik = +number[0];
+document.write(birlik*onlik*yuzlik*minglik + '<br>');
+document.write('Kiritgan soningizni raqamlar kopaytmasi.');
